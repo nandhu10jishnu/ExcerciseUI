8e02abbb62c79d0b72f6f55247511dbdd0db18f3
Arun M. S <aruns@datamatica.uk>
2023-10-25 13:47:43 +0530

    added sample code

---
+++ src/components/createexcercise/CreateExcercise.js
@@ -0,0 +1,20 @@
+export default {
+  name: "createExcercise",
+ 
+  data() {
+    return {
+      BookmarkInsertCommand: {
+        bookmarkName: "",
+        doctypeId: "",
+        sortOrder: 0,
+      },
+    
+    };
+  },
+  async mounted() {
+    console.log("");
+  },
+  methods: {
+    
+  },
+};
---
+++ src/components/createexcercise/CreateExcercise.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="center-container">
+    <div class="container">
+      <div class="col-4">
+        <input type="text" class="form-control" placeholder="City" />
+      </div>
+      <div class="col-4">
+        <input type="text" class="form-control" placeholder="State" />
+      </div>
+      <div class="col-4">
+        <input type="text" class="form-control" placeholder="Zip" />
+      </div>
+    </div>
+  </div>
+</template>
+  <script src="../createexcercise/CreateExcercise.js"></script>
+  <style>
+body {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  margin: 0;
+}
+
+.center-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.container {
+  display: flex;
+}
+
+.col-4 {
+  flex: 1;
+  margin: 5px;
+}
+</style>
